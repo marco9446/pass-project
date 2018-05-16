7dd6cf499f2dcd14e65db5452c7c7cafff42141f
marco ravazzini <marcoravazzini@public-docking-hpx-1306.ethz.ch>
2018-05-16 17:20:18 +0200

    code refactoring

--- flavia_the_best.sh
+++ flavia_the_best.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+if [ -e ./out.dl ]; then
+    rm out.dl
+fi
 
-rm prov.dl
 a="./test/test"
 c=".txt"
 d=$a$1$c
 
-offest=(wc -l $d)
-echo "offset:" $offset
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+line=`wc -l $d | grep -oE '^\s*[0-9]+'`
+echo -e "${YELLOW}Error line offset: ${line} ${NC}\n" 
 
-cat $d "final.dl" >> prov.dl
-souffle prov.dl
-rm prov.dl
+cat $d "final.dl" >> out.dl
+souffle out.dl
+rm out.dl
 echo
 echo
 grep "//" $d
--- marco_the_best.sh
+++ marco_the_best.sh
@@ -6,6 +6,6 @@ a="./test/test"
 b= $2
 c=".txt"
 d=$a$b$c
-cat $d "the_greatest_datalog_program_ever.dl" >> prov.dl
-souffle prov.dl
-rm prov.dl
+cat $d "the_greatest_datalog_program_ever.dl" >> out.dl
+souffle out.dl
+rm out.dl
