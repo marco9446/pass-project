4edad03c5d7e720c2b8555572d0ed6af9b2b9a59
Fla <cflavia@student.ethz.ch>
2018-05-18 14:38:05 +0200

    updates

--- flavia_the_best.sh
+++ flavia_the_best.sh
@@ -13,7 +13,7 @@ NC='\033[0m' # No Color
 line=`wc -l $d | grep -oE '^\s*[0-9]+'`
 echo -e "${YELLOW}Error line offset: ${line} ${NC}\n" 
 
-cat $d "final.dl" >> out.dl
+cat $d "paths.dl" >> out.dl
 souffle out.dl
 rm out.dl
 echo
