7618eeb2df77eabb32006dee43af9426c2f3a737
Fla <cflavia@student.ethz.ch>
2018-06-05 23:51:15 +0200

    miiiiiinchia

---
+++ flavia_the_best.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -e ./out.dl ]; then
+    rm out.dl
+fi
+
+a="./test/test"
+c=".txt"
+d=$a$1$c
+
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+line=`wc -l $d | grep -oE '^\s*[0-9]+'`
+echo -e "${YELLOW}Error line offset: ${line} ${NC}\n" 
+
+cat $d "paths.dl" >> out.dl
+souffle -w out.dl
+rm out.dl
+echo
+echo
+grep "//" $d
+
